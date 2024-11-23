39641d498cc09e95c76e9bdf917fd8eb0787adc9
choonsiong <choonsiong.lee@gmail.com>
2024-11-23 16:45:31 +0800

    Auto commit

---
+++ puppet-server/data/tmp/apache-class.pp
@@ -0,0 +1,20 @@
+class apache {
+  $version = '2.2.3'
+  $docroot = '/export/sites/default'
+  $bindaddress = '10.0.1.15'
+  $port = 80
+
+  package { 'httpd':
+    ensure => $version,
+  }
+
+  $params = {
+    'docroot' => $docroot,
+    'bindaddress' => $bindaddress,
+  }
+
+  file { '/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    content => epp('apache/httpd.conf.epp', $params)
+  }
+}
